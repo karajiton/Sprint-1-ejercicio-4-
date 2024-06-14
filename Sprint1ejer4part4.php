4012b9507d3e2bcd0fedb078549eecce8c1f559f
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-06-14 17:21:19 +0200

    Add files via upload
    ejercicio 4

---
+++ Sprint1ejer4part4.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <?php
+        $infoPersonal = [
+            'nombre' => 'Jonathan Caballero',
+            'edad' => 26,
+            'email' => 'jonathancaballero916@gmail.com',
+            'comida favorita' => 'Pollo Frito'
+        ];
+        print_r ($infoPersonal);
+        ?>
+    </head>
+</html>
